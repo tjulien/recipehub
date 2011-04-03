581929cc6476ad874c92ac96bc310201b4f9adb8
Ubuntu <ubuntu@ip-10-99-87-7.ec2.internal>
2011-04-03 07:03:27 +0000

    adding models; adding .gitignore; enabling admin

---
+++ foodhub/admin.py
@@ -0,0 +1,10 @@
+from foodhub.models import *
+from django.contrib import admin
+
+admin.site.register(Recipe)
+admin.site.register(User)
+admin.site.register(Ingredient)
+admin.site.register(IngredientInstance)
+admin.site.register(Follower)
+admin.site.register(Unit)
+admin.site.register(RecipeFork)
---
+++ foodhub/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+    password = models.CharField(max_length=32)
+
+class Follower(models.Model):
+    follower = models.ForeignKey(User, related_name='follower')
+    following = models.ForeignKey(User, related_name='following')
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=32)
+
+class Unit(models.Model):
+    name = models.CharField(max_length=16)
+
+class IngredientInstance(models.Model):
+    ingredient = models.ForeignKey(Ingredient)
+    num = models.IntegerField()
+    units = models.ForeignKey(Unit)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=64)
+    user = models.ForeignKey(User)
+    create_date = models.DateTimeField('date created')
+    ingredients = models.ManyToManyField(IngredientInstance)
+
+class RecipeFork(models.Model):
+    fork = models.ForeignKey(Recipe, related_name='fork')
+    forked = models.ForeignKey(Recipe, related_name='forked')
