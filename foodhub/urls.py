9b0394855da371ea62f83b38df2216c97cf953ca
Tim Julien <tim.julien@gmail.com>
2011-04-09 17:58:26 +0000

    adding login.html

--- foodhub/urls.py
+++ foodhub/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^register', 'foodhub.views.register', name='register'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     url(r'^$', 'foodhub.views.index', name='index'),    
     # url(r'^recipehub/', include('recipehub.foo.urls')),
 )
